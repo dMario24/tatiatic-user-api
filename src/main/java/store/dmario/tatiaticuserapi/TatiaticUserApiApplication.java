5592fecb2eb0d0d62cb51aea99f0c341e86f8438
dmario24 <data.mario24@gmail.com>
2023-08-07 10:30:36 +0900

    init

---
+++ src/main/java/store/dmario/tatiaticuserapi/TatiaticUserApiApplication.java
@@ -0,0 +1,13 @@
+package store.dmario.tatiaticuserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatiaticUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatiaticUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/store/dmario/tatiaticuserapi/TatiaticUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package store.dmario.tatiaticuserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatiaticUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
